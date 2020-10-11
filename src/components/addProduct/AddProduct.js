d0532738c1dadc1012cdabe3b0cd8756d070c9fa
David Sequera <davidsequeralozada@gmail.com>
2020-10-11 10:02:49 -0430

    Styled the app a little bit

--- src/components/addProduct/AddProduct.js
+++ src/components/addProduct/AddProduct.js
@@ -3,6 +3,7 @@ import React from 'react';
 const AddProduct = () => {
     return (
         <div>
+            <h4>Add a Product</h4>
         <form action="http://localhost:4000/products/addProduct" method="POST">
             <input type="text" placeholder="Enter product name" name="name" required></input>
             <input type="text" placeholder="Enter product price" name="price" required></input>
@@ -16,7 +17,8 @@ const AddProduct = () => {
            
             </form>
 
-           
+  <h4>Add many products : Upload a excel file</h4> 
+ 
 <form action="http://localhost:4000:/products/addMany" method="POST" encType="multipart/form-data">
     <div >
         <input type="file" name="excel"/>
